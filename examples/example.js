3b343f55450fe589c2690deb9a4a2ef4e6568628
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2017-12-26 23:15:58 +0530

    final

--- examples/example.js
+++ examples/example.js
@@ -1,2 +1,6 @@
-var everytime = require('../.')()
-console.log(everytime.getTime(82.5644, true))
+var everytime = require('@shubhodeep9/everytime')()
+// simple usage
+console.log(everytime.getTime(82.5644)) // prints time as a Date object eg: 2017-12-26T23:03:28.402Z
+
+// formatting
+console.log(everytime.getTime(82.5644, true)) // prints time as Date string eg: 'Wed, 27 Dec 2017 04:33:28 GMT'
